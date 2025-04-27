b0c993f0e82afa2d4d0c703fb78749c459eb3874
Arvin Singh <hgnis.nivra@hotmail.com>
2025-04-27 21:24:01 +0100

    fix: removed unnecessary feats

--- app/components/icons/index.js
+++ app/components/icons/index.js
@@ -1,7 +1,6 @@
 export { default as GitHub } from './github'
 export { default as Twitter } from './twitter'
 export { default as Mail } from './mail'
-export { default as RSS } from './rss'
 export { default as LinkedIn } from './linkedin'
 export { default as Star } from './star'
 export { default as Sun } from './sun'
