e701e90cdd2ed7411f1c7222be5fc05c06e87c45
Arvin Singh <hgnis.nivra@hotmail.com>
2026-05-20 11:18:46 +0100

    update robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,48 @@ import { getContentConfig, getWebsiteUrl } from './data/content-config'
 export default async function robots() {
   const contentConfig = await getContentConfig()
   const websiteUrl = getWebsiteUrl(contentConfig)
+
   return {
     rules: [
       {
+        userAgent: 'ia_archiver',
+        disallow: '/',
+      },
+      {
+        userAgent: 'CCBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Applebot-Extended',
+        disallow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Meta-ExternalAgent',
+        disallow: '/',
+      },
+      {
+        userAgent: 'meta-externalagent',
+        disallow: '/',
+      },
+      {
         userAgent: '*',
-        allow: ['/'],
+        allow: '/',
       },
     ],
     sitemap: `${websiteUrl}/sitemap.xml`,
